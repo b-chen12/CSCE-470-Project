9a58d3a6633c183b784386dcfa4cd04bc0c9861b
Benson Nguyen <79603955+628005910@users.noreply.github.com>
2023-11-09 20:38:19 -0600

    add recipe endpoints to backend

--- BE/api.py
+++ BE/api.py
@@ -1,10 +1,90 @@
+import requests
 from fastapi import FastAPI
+from sklearn.metrics.pairwise import cosine_similarity
+
+API_KEY = '47239b1c022f44d1b8c885f71fd373ea'
 
 app = FastAPI()
 
-@app.get('/hello')
-async def hello_world():
-    return {"message": "Hello World"}
+ATTRIBUTES_CONSIDERED = ['vegetarian', 'vegan', 'veryHealthy', 'dairyFree', 'dairyFree'] + ['healthScore', 'pricePerServing']
+
+def get_recipe_details(id: str):
+    url = f"https://api.spoonacular.com/recipes/{id}/information"
+    params = {"apiKey": API_KEY}
+    response = requests.get(url, params=params)
+    return response.json() if response.status_code == 200 else None
+
+def extract_ingredients(recipes):
+    all_ingredients = set()
+
+    for recipe in recipes:
+        for ingredient in recipe.get('extendedIngredients', []):
+            all_ingredients.add(ingredient['name'])
+
+    return list(all_ingredients)
+
+def get_random_recipes(number_of_recipes: int):
+    url = f"https://api.spoonacular.com/recipes/random"
+    params = {
+        "apiKey": API_KEY,
+        "number": number_of_recipes
+    }
+    response = requests.get(url, params=params)
+    return response.json()['recipes'] if response.status_code == 200 else None
+
+def create_feature_vector(recipe, all_ingredients):
+    HIGH_WEIGHT = 1.0
+    MED_WEIGHT = 0.8
+    LOW_WEIGHT = 0.3
+
+    vector = []
+
+    high_importance_attributes = ['vegetarian', 'vegan', 'veryHealthy', 'dairyFree', 'dairyFree']
+    for attribute in high_importance_attributes:
+        vector.append((1 if recipe.get(attribute, False) else 0) * HIGH_WEIGHT)
+
+    ingredients_in_recipe = [ingredient['name'] for ingredient in recipe.get('extendedIngredients', [])]
+    for ingredient in all_ingredients:
+        vector.append((1 if ingredient in ingredients_in_recipe else 0) * MED_WEIGHT)
+
+    low_importance_features = ['healthScore', 'pricePerServing']
+    for feature in low_importance_features:
+        value = recipe.get(feature, 0)
+        normalized_value = value / 100
+        vector.append(normalized_value * LOW_WEIGHT)
+
+    return vector
+
+@app.get("/fetch_random_recipes/{number_of_recipes}")
+def fetch_random_recipes(number_of_recipes: int):
+    url = f"https://api.spoonacular.com/recipes/random"
+    params = {
+        "apiKey": API_KEY,
+        "number": number_of_recipes
+    }
+    response = requests.get(url, params=params)
+    return response.json()['recipes'] if response.status_code == 200 else None
+
+@app.get("/fetch_recipe_details/{id}")
+async def fetch_recipe_details(id: str):
+    return get_recipe_details(id)
+
+@app.get("/get_similar_recipes/{id}")
+async def get_similar_recipes(id: str):
+    target_recipe_details = get_recipe_details(id)
+    comparing_recipes_details = get_random_recipes(100)
+    all_ingredients = extract_ingredients(comparing_recipes_details + [target_recipe_details])
+
+    target_vector = create_feature_vector(target_recipe_details, all_ingredients)
+    similarities = []
+
+    for recipe in comparing_recipes_details:
+        recipe_vector = create_feature_vector(recipe, all_ingredients)
+        similarity = cosine_similarity([target_vector, recipe_vector])[0][1]
+        similarities.append((recipe, similarity))
+
+    top_similar_recipes = sorted(similarities, key=lambda x: x[1], reverse=True)[:10]
+    return [recipe for recipe,_ in top_similar_recipes]
 
 if __name__ == "__main__":
     import uvicorn
