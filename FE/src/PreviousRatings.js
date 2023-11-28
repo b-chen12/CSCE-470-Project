8c662a98e53857aa05aec1da5fa41b641178d63e
b-chen12 <brianc2002@gmail.com>
2023-11-28 16:34:12 -0600

    added code to display previous ratings

--- FE/src/PreviousRatings.js
+++ FE/src/PreviousRatings.js
@@ -1,17 +1,50 @@
-// src/PreviousRatings.js
-import React from 'react';
+// src/UserRatingsPage.js
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './RecipePage.css';
 
-const PreviousRatings = () => {
+const UserRatingsPage = () => {
+  const [userRatings, setUserRatings] = useState([]);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    // Fetch user ratings based on the username from localStorage
+    const fetchUserRatings = async () => {
+      const userName = localStorage.getItem('userName');
+      
+      try {
+        const response = await fetch(`http://localhost:8000/userRatings/${userName}`);
+        if (response.ok) {
+          const data = await response.json();
+          setUserRatings(data);
+        } else {
+          console.error('Failed to fetch user ratings');
+        }
+      } catch (error) {
+        console.error('Error fetching user ratings:', error);
+      }
+    };
+
+    fetchUserRatings();
+  }, []);
+
   return (
-    <div className="recipe-page">
+    <div className="user-ratings-page">
       <button onClick={() => navigate('/welcome')} className="back-button">Back to Welcome</button>
       <h1 className="page-title">Previous Ratings</h1>
+
+      {userRatings.length === 0 ? (
+        <p>No ratings found for the user.</p>
+      ) : (
+        <ul>
+          {userRatings.map((rating) => (
+            <li key={rating.RecipeID}>
+              Recipe ID: {rating.RecipeID}, Rating: {rating.Rating}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default PreviousRatings;
+export default UserRatingsPage;
