0a8d22ea37371fcf562ff17f660369f7032f5bd5
b-chen12 <brianc2002@gmail.com>
2023-11-28 16:42:29 -0600

    Displayed rating names instead of rating ids

--- FE/src/PreviousRatings.js
+++ FE/src/PreviousRatings.js
@@ -4,18 +4,21 @@ import { useNavigate } from 'react-router-dom';
 
 const UserRatingsPage = () => {
   const [userRatings, setUserRatings] = useState([]);
+  const [recipeDetails, setRecipeDetails] = useState({});
   const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch user ratings based on the username from localStorage
     const fetchUserRatings = async () => {
       const userName = localStorage.getItem('userName');
-      
+
       try {
         const response = await fetch(`http://localhost:8000/userRatings/${userName}`);
         if (response.ok) {
           const data = await response.json();
           setUserRatings(data);
+          // Fetch details for each recipe ID
+          data.forEach((rating) => fetchRecipeDetails(rating.RecipeID));
         } else {
           console.error('Failed to fetch user ratings');
         }
@@ -24,24 +27,45 @@ const UserRatingsPage = () => {
       }
     };
 
+    const fetchRecipeDetails = async (recipeID) => {
+      try {
+        const response = await fetch(`http://localhost:8000/fetch_recipe_details/${recipeID}`);
+        if (response.ok) {
+          const data = await response.json();
+          setRecipeDetails((prevDetails) => ({ ...prevDetails, [recipeID]: data.title }));
+        } else {
+          console.error(`Failed to fetch details for recipe ID ${recipeID}`);
+        }
+      } catch (error) {
+        console.error(`Error fetching details for recipe ID ${recipeID}:`, error);
+      }
+    };
+
     fetchUserRatings();
   }, []);
 
   return (
     <div className="user-ratings-page">
-      <button onClick={() => navigate('/welcome')} className="back-button">Back to Welcome</button>
+      <button onClick={() => navigate('/welcome')} className="back-button">
+        Back to Welcome
+      </button>
       <h1 className="page-title">Previous Ratings</h1>
 
       {userRatings.length === 0 ? (
         <p>No ratings found for the user.</p>
       ) : (
-        <ul>
+        <div className="recipes-container">
           {userRatings.map((rating) => (
-            <li key={rating.RecipeID}>
-              Recipe ID: {rating.RecipeID}, Rating: {rating.Rating}
-            </li>
+            <div
+              key={rating.RecipeID}
+              className="recipe"
+              onClick={() => navigate(`/recipes/${rating.RecipeID}`)}
+            >
+              <h3>{recipeDetails[rating.RecipeID]}</h3>
+              <p>Rating: {rating.Rating}</p>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
